4fb475c83bbe578995ad630bcc6dfaaa11008348
c0a21159 <c0a21159be@edu.teu.ac.jp>
2022-11-29 14:34:34 +0900

    練習4

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,10 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-#練習1
-root = tk.Tk()
-root.title("tk")
-root.geometry("300x500")
 
 #練習3
 def button_click(event):
@@ -12,16 +8,31 @@ def button_click(event):
     num = btn["text"]
     tkm.showinfo("", f"{num}ボタンがクリックされました")
 
+
+#練習1
+root = tk.Tk()
+root.title("tk")
+root.geometry("300x500")
+
+
+#練習4
+entry  = tk.Entry(root, justify="right", width=10, font=("", 40))
+entry.grid(row=0, column=0, columnspan=3)
+
+
 #練習2
-r, c = 0, 0
+r, c = 1, 0
 for num in range(9, -1, -1):
-    for i in range(3):
-        button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
-        button.grid(row= r, column=c)
-        button.bind("<1>", button_click)
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row= r, column=c)
+    button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
         c = 0
+
+
+
+
 
 root.mainloop()
