e8237c2c6e535a2cb61c6d0f7fbb1cd1c6a05d18
c0a21159 <c0a21159be@edu.teu.ac.jp>
2022-11-29 14:24:07 +0900

    練習3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,16 +1,24 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
-#練習
+#練習1
 root = tk.Tk()
 root.title("tk")
 root.geometry("300x500")
+
+#練習3
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo("", f"{num}ボタンがクリックされました")
 
 #練習2
 r, c = 0, 0
 for num in range(9, -1, -1):
     for i in range(3):
-        button9 = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
-        button9.grid(row= r, column=c)
+        button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+        button.grid(row= r, column=c)
+        button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
