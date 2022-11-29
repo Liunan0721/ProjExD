814e92fce9ead9a39cae733dd9f2c2cfba9b32dd
c0a21159 <c0a21159be@edu.teu.ac.jp>
2022-11-29 17:01:40 +0900

    追加機能

--- ex02/calc.py
+++ ex02/calc.py
@@ -12,11 +12,37 @@ def button_click(event):
         res = eval(siki) # 数式文字列を評価する
         entry.delete(0, tk.END) # 表示文字列の削除
         entry.insert(tk.END, res) # 結果の挿入
+
+    elif num in function:
+        siki = entry.get()
+        if num == "C":
+            entry.delete(0, tk.END) 
         
-    else: #[=]以外の文字列
+        if num == "←":
+            entry.delete(len(siki)-1, tk.END) # 表示文字列の一桁を消す
+
+        if num == "x^2":
+            siki = entry.get()
+            res = float(siki)**2
+            entry.delete(0,tk.END)
+            entry.insert(tk.END, res)
+
+        if num == "1/x":
+            siki = entry.get()
+            res = 1/float(siki)
+            entry.delete(0,tk.END)
+            entry.insert(tk.END, res)
+
+
+
+
+    else: #[=]とclearリスト以外の文字列
         #tkm.showinfo("", f"{num}ボタンがクリックされました")
         #練習6
+
         entry.insert(tk.END, num)
+
+
 
 #練習1
 root = tk.Tk()
@@ -26,30 +52,51 @@ root.geometry("300x500")
 
 #練習4
 entry  = tk.Entry(root, justify="right", width=10, font=("", 40))
-entry.grid(row=0, column=0, columnspan=3)
+entry.grid(row=0, column=0, columnspan=4)
+
+
+r, c = 1, 0
+function = ["%", "CE", "C", "←", "1/x", "x^2", "√x"]
+for fun in function:
+    button = tk.Button(root, text=f"{fun}", width=3, height=1, font=("", 30))
+    button.grid(row= r, column=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c % 4 == 0:
+        r+=1
+        c=0
 
 
 #練習2
-r, c = 1, 0
+r, c = 3, 2
 for num in range(9, -1, -1):
-    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    if num == 0:
+        r, c = 6, 0
+    button = tk.Button(root, text=f"{num}", width=3, height=1, font=("", 30))
     button.grid(row= r, column=c)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
+    c -= 1
+    if c == -1:
         r += 1
-        c = 0
+        c = 2
+button = tk.Button(root, text=".", width=3, height=1, font=("", 30))
+button.grid(row= 6, column=1)
+button.bind("<1>", button_click)
+    
+
 
 
 #練習5
-operators = ["+", "="]
+r, c = 2, 3
+operators = ["+", "-", "*", "/", "="]
 for ope in operators:
-    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
+    button = tk.Button(root, text=f"{ope}", width=3, height=1, font=("", 30))
     button.grid(row= r, column=c)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
-        r += 1
-        c = 0
+    r += 1
+
+
+
+
 
 root.mainloop()
