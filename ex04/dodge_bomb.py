161347677780619590826f10c11e8c89a330f752
c0a21159 <c0a21159be@edu.teu.ac.jp>
2022-12-13 14:17:08 +0900

    練習4-2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,10 +3,24 @@ import sys
 
 
 def main():
+    clock = pg.time.Clock()
+
+    #練習1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
 
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+    pgbg_rct = pgbg_sfc.get_rect()
+    
+    #練習2
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.update()
+        clock.tick(1000)
+
 
 
 if __name__ == "__main__":
