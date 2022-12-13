1603cf67e6c679dba2fcedb1ecdce1e0a4519ae8
c0a21159 <c0a21159be@edu.teu.ac.jp>
2022-12-13 14:47:17 +0900

    練習4-5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import sys
-
+import random
 
 def main():
     clock = pg.time.Clock()
@@ -8,7 +8,7 @@ def main():
     #練習1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
-
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
     
@@ -20,10 +20,20 @@ def main():
     #scrn_sfcにtori_rctに従って,tori_sfcを貼り付ける
     scrn_sfc.blit(tori_sfc, tori_rct)
 
+    #練習5
+    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct)
+
 
     #練習2
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
@@ -41,6 +51,7 @@ def main():
         scrn_sfc.blit(tori_sfc, tori_rct)
 
 
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
         pg.display.update()
         clock.tick(1000)
 
